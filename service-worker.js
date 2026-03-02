a05fbb82b10a36f311edd757ed2ebccb18dd7577
valentim5102000-tech <valentim5102000@gmail.com>
2026-03-03 00:16:12 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'localwab-cache-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
